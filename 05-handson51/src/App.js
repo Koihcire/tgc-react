dd4e7c161c1974dbb0e5db49a9dd7a8883d93a08
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-06-27 21:55:04 +0800

    handson51

---
+++ 05-handson51/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ContactForm from "./ContactForm";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Contact Us Form</h1>
+      <ContactForm/>
+
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ 05-handson51/src/ContactForm.js
@@ -0,0 +1,55 @@
+import React from "react"
+
+export default class ContactForm extends React.Component{
+
+    state = {
+        "firstName": "",
+        "lastName": "",
+        "enquiry": "",
+        "country": ""
+    }
+
+    updateFormField = (e) =>{
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    summary = () =>{
+        alert(`Name: ${this.state.firstName} ${this.state.lastName}
+        Enquiry: ${this.state.enquiry}
+        Country: ${this.state.country}`
+        )
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <div>
+                    <label>First Name:</label>
+                    <input type="text" name="firstName" className="form-control" value={this.state.firstName} onChange={this.updateFormField}/>
+                </div>
+                <div>
+                    <label>Last Name:</label>
+                    <input type="text" name="lastName" className="form-control" value={this.state.lastName} onChange={this.updateFormField}/>
+                </div>
+                <div>
+                    <label>Country:</label>
+                    <input type="radio" name="enquiry" value="support" checked={this.state.enquiry === "support"} onChange={this.updateFormField}/>Support 
+                    <input type="radio" name="enquiry" value="sales" checked={this.state.enquiry === "sales"} onChange={this.updateFormField}/>Sales 
+                    <input type="radio" name="enquiry" value="marketing" checked={this.state.enquiry === "marketing"} onChange={this.updateFormField}/>Marketing
+                </div>
+                <div>
+                    <label>Country</label>
+                    <select name="country" value={this.state.country} onChange={this.updateFormField}>
+                        <option value="singapore">Singapore</option>
+                        <option value="malaysia">Malaysia</option>
+                        <option value="thailand">Thailand</option>
+                    </select>
+                </div>
+                <button onClick={this.summary}>Submit</button>
+
+            </React.Fragment>
+        )
+    }
+}

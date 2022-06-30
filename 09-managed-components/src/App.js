f9d5601e1733b7df6cf731b28761960686952223
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-06-30 17:29:43 +0800

    just added

---
+++ 09-managed-components/src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import SignupForm from "./SignupForm";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+function App() {
+  return (
+   <React.Fragment>
+    <h1>Managed Components</h1>
+    
+    <SignupForm/>
+   </React.Fragment>
+  );
+}
+
+export default App;

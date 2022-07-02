02ef0db70688c4cf36e4abf40be628f0dd5130ba
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-07-02 21:00:27 +0800

    update

---
+++ 10-single-page-navigation/src/RecipeBook.js
@@ -0,0 +1,10 @@
+import React from "react"
+export default class RecipeBook extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                
+            </React.Fragment>
+        )
+    }
+}
